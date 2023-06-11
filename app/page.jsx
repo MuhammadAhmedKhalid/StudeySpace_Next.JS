9cdafda4955db0fca79fba6997769c546ce18487
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-06-11 17:23:19 +0500

    Home page styling completed.

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,13 @@ import '../styles/home.css'
 export default function Home() {
 
     return (
-        <div>
-            Study Space
+        <div className='center'>
+            <h1>Study Space</h1>
+            <p>Streamline education with our interactive platform for teachers and students, 
+                enabling effortless classroom management, online quizzes, and assignment submissions.</p>
+            <button className='rounded-button'>
+                Contact Us
+            </button>
         </div>
     )
 }
