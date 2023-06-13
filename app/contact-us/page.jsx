7bc8cc55937ccea31440c70ecfaeb5794d51dbff
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-06-13 23:58:08 +0500

    Responsiveness added for contact us page.

--- app/contact-us/page.jsx
+++ app/contact-us/page.jsx
@@ -1,23 +1,34 @@
+'use client';
+
 import ContactForm from '@components/ContactForm'
+import ContactContent from '@components/ContactContent';
 
 function ContactUs() {
+
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <>
-      <div className='bg-color'>
-        <div className="container">
-          <div className="left-portion y-axis">
-            <h1 className='blue-color'>CONTACT US</h1>
-              <p className='blue-color p-alignment'>
-                We value your feedback and are committed to providing excellent support. If you have any questions, suggestions, or need assistance, 
-                please don't hesitate to reach out to us. Our dedicated support team is here to help you make the most of our web application. 
-                We appreciate your interest in our platform and look forward to assisting you!
-              </p>
+      {
+        isMobile ? 
+        <>
+          <div className='bg-color container y-axis'>
+            <ContactContent/>
           </div>
-          <div className='right-portion'>
-            <ContactForm/>
+          <ContactForm/>
+        </>
+        :
+        <div className='bg-color'>
+          <div className="container">
+            <div className="left-portion y-axis">
+              <ContactContent/>
+            </div>
+            <div className='right-portion'>
+              <ContactForm/>
+            </div>
           </div>
         </div>
-      </div>
+      }
     </>
   )
 }
---
+++ components/ContactContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ContactContent() {
+  return (
+    <>
+        <h1 className='blue-color'>CONTACT US</h1>
+        <p className='blue-color p-alignment'>
+            We value your feedback and are committed to providing excellent support. If you have any questions, suggestions, or need assistance, 
+            please don't hesitate to reach out to us. Our dedicated support team is here to help you make the most of our web application. 
+            We appreciate your interest in our platform and look forward to assisting you!
+        </p>
+    </>
+  )
+}
+
+export default ContactContent
