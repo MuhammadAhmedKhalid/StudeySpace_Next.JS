47ec2ea5f6635a8df34e81735456e866f08e10b8
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-06-13 16:43:17 +0500

    Footer component updated.

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,16 @@ import '../styles/footer.css'
 
 function Footer() {
   return (
-    <div style={{background: '#0a002c', height: '200px'}}>
-      <p>Footer</p>
-    </div>
+    <footer>
+       <div className='x-axis'>
+          <img alt='fb_icon' src='./icons/fb.png' className='logo'/>
+          <img alt='twitter_icon' src='/icons/twitter.png' className='logo'/>
+          <img alt='insta_icon' src='/icons/insta.png' className='logo'/>
+        </div>
+        <div>
+          <p>© 2023 Study Space. All rights reserved.</p>
+        </div>
+    </footer>
   )
 }
 
