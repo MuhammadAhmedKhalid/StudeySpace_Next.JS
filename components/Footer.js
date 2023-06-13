b3eee166c945c153bd98bbbc52557f4732e69ffe
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-06-13 18:20:58 +0500

    About us page has been updated.

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
        <div className='x-axis'>
-          <img alt='fb_icon' src='./icons/fb.png' className='logo'/>
+          <img alt='fb_icon' src='/icons/fb.png' className='logo'/>
           <img alt='twitter_icon' src='/icons/twitter.png' className='logo'/>
           <img alt='insta_icon' src='/icons/insta.png' className='logo'/>
         </div>
