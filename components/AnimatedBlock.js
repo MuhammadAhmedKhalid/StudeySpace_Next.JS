3a9dbe1322ccc0bbe0ad866b176aca9f05863242
MUHAMMAD AHMED <muhammadahmedkhalid4@gmail.com>
2023-07-04 22:44:43 +0500

    Buttons added on slide-up block.

--- components/AnimatedBlock.js
+++ components/AnimatedBlock.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import '../styles/ainmated-block.css'
+import AddIcon from '@mui/icons-material/Add';
+import { BiHash } from 'react-icons/bi';
 
 function AnimatedBlock({isBlockVisible, blockRef}) {
     return (
         <div
             className={`animated-block ${isBlockVisible ? 'show' : ''}`}
             ref={blockRef}
-        />
+        >
+            <div className='row'>
+                <AddIcon className='row-icon'/>
+                <h2 className='row-text'>Create new class</h2>
+            </div>
+            <div className='row'>
+                <BiHash size={21} color='#c72e2e' style={{marginRight: '10px'}}/>
+                <h2 className='row-text'>Join a class with a code</h2>
+            </div>
+        </div>
     )
 }
 
